56fb4973b5d3f603e2b779a33907d806e4132aa9
CuaKid4Teen <justin.cua14@gmail.com>
2019-01-12 14:41:25 -0800

    intial commit

---
+++ src/main/java/frc/robot/subsystems/FlyWheel.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Add your docs here.
+ */
+public class FlyWheel extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+private final TalonSRX raven; 
+private final VictorSPX rave;
+
+  public FlyWheel() {
+//constructor, only runs once
+raven = new TalonSRX(0);
+
+}
+
+
+
+public void setPower(/*parameter called power*/double power){
+/*setPower is a method*/
+raven.set(speed);
+raven.set(power);
+//needs controlMode.percentOutput or TalonSRX, sets % of maximum voltage to motor
+
+}
+
+public void setLowPower(/*No parameter*/){
+//command used to set motor to 10% or .1 power
+    setPower(.1);
+
+}
+
+public void stopPower(){
+//safety measure to stop power
+setPower(0);
+
+}
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+    //method example
+  }
+}
